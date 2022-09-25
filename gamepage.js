a693b25469e00ff481bd0aefc15ce2864c85b50a
jigyashagit <109466868+jigyashagit@users.noreply.github.com>
2022-09-25 17:44:21 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,28 @@
+var y  = Math.floor(Math.random()*10+1);
+var guess=1;
+playername=localStorage.getItem("player_name");
+function submit(){
+    var x =document.getElementById("guessField").value ;
+
+
+if(x==y)
+{
+ alert("CONGRATULATIONS!!! "+playername+" YOU GUESSED IT RIGHT IN "+guess+" GUESSES");
+ guess=1;
+}
+else if(x>y)
+{
+    guess++;
+    alert("!OOPS SORRY!! TRY A SMALLER NUMBER!")
+}
+else
+{
+    guess++;
+    alert("!OOPS SORRY!! TRY A GREATER NUMBER!")
+}
+}
+
+function playagain()
+{
+    y  = Math.floor(Math.random()*10+1);
+}
